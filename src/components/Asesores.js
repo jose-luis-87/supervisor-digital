172936bcd180131f2f7850728f34e253d5f41366
MandujanoJoseLuis <jose.luis@codesign.mx>
2020-12-14 16:29:21 -0600

    Ajustes secion asesor cards

--- src/components/Asesores.js
+++ src/components/Asesores.js
@@ -1,57 +1,98 @@
 import React from 'react';
 import imgUser from '../img/user.jpg';
+import { ProgressBar } from './progressbar/ProgressBar';
+
+import 'swiper/swiper.scss';
+
 const data = [
      {
           id: 1,
           name: 'Gerardo López',
-          puntoventa: 'Plutarco',
-          promedio: 68,
+          puntoventa: 'PLUTARCO',
+          promedio: 98,
           img: {imgUser},
+          bgcolor: "#00C853"
+          
      },
      {
           id: 2,
-          name: 'Gerardo López',
-          puntoventa: 'Plutarco',
-          promedio: 68,
+          name: 'Eduardo Ruiz',
+          puntoventa: 'CENTENARIO',
+          promedio: 78,
           img: '../img/user.jpg',
+          bgcolor: "#0061B2"
      },
      {
           id: 3,
-          name: 'Gerardo López',
-          puntoventa: 'Plutarco',
+          name: 'Ana Madrid',
+          puntoventa: 'GRANJAS',
           promedio: 68,
           img: '../img/user.jpg',
+          bgcolor: "#0061B2"
      },
      {
           id: 4,
-          name: 'Gerardo López',
-          puntoventa: 'Plutarco',
-          promedio: 68,
-          img: '../img/user.jpg'
+          name: 'Álan Castro',
+          puntoventa: 'NORTE 25',
+          promedio: 45,
+          img: '../img/user.jpg',
+          bgcolor: "#FF8A01"
+     },
+     {
+          id: 5,
+          name: 'Brenda Cloud',
+          puntoventa: 'ATLANTA',
+          promedio: 45,
+          img: '../img/user.jpg',
+          bgcolor: "#FF8A01"
+     },
+     {
+          id: 6,
+          name: 'Álan Castro',
+          puntoventa: 'NORTE 25',
+          promedio: 45,
+          img: '../img/user.jpg',
+          bgcolor: "#FF8A01"
+     },
+     {
+          id: 7,
+          name: 'Brenda Cloud',
+          puntoventa: 'ATLANTA',
+          promedio: 45,
+          img: '../img/user.jpg',
+          bgcolor: "#FF8A01"
      }
 ]
+
 function Asesores() {
+    
      
      return (
-          <div>
-               {data.map(asesor=>(
-                    
-                    <div className="cardUser">
-                         <div className="user">
-                              <div className="avatar">
-                                   <img src={imgUser} alt={"avatar"}/>
-                              </div>
-                              <div className="datos">
-                                   <h2>{asesor.name}</h2>
-                                   <p>{asesor.puntoventa}</p>
-                              </div>
-                              
-                         </div> 
-                    </div>
-               )
+          
+               <div>
+                    {data.map(asesor=>(
+                         
 
-               )}
-          </div>
+                         
+                              <div className="cardUser">
+                                   <div className="user">
+                                        <div className="avatar">
+                                             <img src={imgUser} alt={"avatar"}/>
+                                        </div>
+                                        <div className="datos">
+                                             <h2>{asesor.name}</h2>
+                                             <p>{`PV. ${asesor.puntoventa}`}</p>
+                                        </div>
+                                   </div> 
+                                   <ProgressBar key={asesor.id} bgcolor={asesor.bgcolor} promedio={asesor.promedio}/>
+                              </div>
+                         
+                         
+                    ))
+               }
+               </div>
+               
+          
      )
 }
 
